cebdc6ee7e1c758833f62fe127e5c875fdacaa4a
ani404 <chhotu.aniket@moneyforward.co.jp>
2023-12-21 00:26:59 +0900

    added more functions

--- src/main/kotlin/com/example/marketo/service/ProductService.kt
+++ src/main/kotlin/com/example/marketo/service/ProductService.kt
@@ -1,10 +1,28 @@
 package com.example.marketo.service
 
+import com.example.marketo.entities.Product
 import com.example.marketo.repository.ProductRepository
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class ProductService(private val productRepository: ProductRepository) {
 
+    fun getAllProducts(): MutableIterable<Product> = productRepository.findAll()
 
+    fun getProductById(id: Long): Optional<Product> = productRepository.findById(id)
+
+    fun updateProduct(product: Product): Product = productRepository.save(product)
+
+    fun createProduct(product: Product): Product {
+        if (product.id != null || product.name != null || product.price != null) {
+            return productRepository.save(product)
+        } else {
+            throw IllegalArgumentException("At least one field in the Product must not be null")
+        }
+    }
+
+    fun deleteProductById(id: Long) {
+        productRepository.deleteById(id)
+    }
 }
